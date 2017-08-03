870a4a92456c056c3244c17e1900304f3f400eec
ut4utc <ut4utc@users.noreply.github.com>
2017-08-03 22:57:24 +0300

    fee2

--- js-circle/main2.js
+++ js-circle/main2.js
@@ -3,9 +3,17 @@ $(document).ready(function() {
 	paper.install(window);
 	paper.setup(document.getElementById('mainCanvas'));
 		var tool = new Tool();
+		var c = Shape.Circle(200,200,80);
+			c.fillColor = 'black';
+		var text = new PointText(200,200);
+			text.fillColor = 'white';
+			text.fontSize = 20;
+			text.content = 'hello wrld';
+
 		tool.onMouseDown = function(event) {
 			var c = Shape.Circle(event.point.x, event.point.y, 20);
-			c.fillColor = 'green';
+				c.fillColor = 'green';
+			console.log('add new object');
 		};
 	paper.view.draw();
 });
