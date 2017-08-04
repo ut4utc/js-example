1dbf3acb082dc88ef29eb181ba8d4ff78e1fa2a6
ut4utc <ut4utc@users.noreply.github.com>
2017-08-04 11:09:18 +0300

    fix3

--- js-circle/main2.js
+++ js-circle/main2.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 	'use strict';
 	paper.install(window);
-	paper.setup(document.getElementById('mainCanvas'));
+	paper.setup(document.getElementById('mainCanvas2'));
 		var tool = new Tool();
 		var c = Shape.Circle(200,200,80);
 			c.fillColor = 'black';
 		var text = new PointText(200,200);
 			text.fillColor = 'white';
 			text.fontSize = 20;
-			text.content = 'hello wrld';
+			text.content = 'hello';
 
 		tool.onMouseDown = function(event) {
 			var c = Shape.Circle(event.point.x, event.point.y, 20);
